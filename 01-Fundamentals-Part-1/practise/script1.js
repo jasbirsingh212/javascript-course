f32c3d36346cea905aa515e13e4a5bdd612bac5e
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2022-01-08 09:35:22 +0530

    completed assessment 1

--- 01-Fundamentals-Part-1/practise/script1.js
+++ 01-Fundamentals-Part-1/practise/script1.js
@@ -1,1 +1,11 @@
+// assessment - 1
 console.log("Hello World!");
+
+//assessment - 2
+let myCountry = 'India';
+let myContinent = 'Asia pacific';
+let populationOfMyContry = 140;
+
+console.log(myCountry);
+console.log(myContinent);
+console.log(`${populationOfMyContry} millions`);
