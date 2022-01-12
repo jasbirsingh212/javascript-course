cd2e0d84167474cad0575f3ad1366659145578d1
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2022-01-12 10:56:51 +0530

    truthy and falsy value

--- 01-Fundamentals-Part-1/practise/script1.js
+++ 01-Fundamentals-Part-1/practise/script1.js
@@ -1,11 +1,15 @@
 // assessment - 1
-console.log("Hello World!");
+//console.log("Hello World!");
 
 //assessment - 2
-let myCountry = 'India';
-let myContinent = 'Asia pacific';
-let populationOfMyContry = 140;
+// let myCountry = 'India';
+// let myContinent = 'Asia pacific';
+// let populationOfMyContry = 140;
 
-console.log(myCountry);
-console.log(myContinent);
-console.log(`${populationOfMyContry} millions`);
+// console.log(myCountry);
+// console.log(myContinent);
+// console.log(`${populationOfMyContry} millions`);
+
+// truthy and falsy values
+//if({}) console.log("true array");
+//if([]) console.log("true array");
