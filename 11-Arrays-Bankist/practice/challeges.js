80d91f1ed848ebb3ba23601d14fcbd4c75ead87f
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2022-02-09 13:30:35 +0530

    completed challenge 1 of this section

--- 11-Arrays-Bankist/practice/challeges.js
+++ 11-Arrays-Bankist/practice/challeges.js
@@ -1,26 +1,17 @@
 // console.log('hello world');
 
-
-const printDogMsg = (arr) => {
-
-    arr.forEach(age => {
-
-        if(age >= 3){
-            console.log(`Dog is Adult, with age: ${age}`)
-        }
-        else{
-            console.log('Dog is Puppy')
-        }
-        
-    });
-
-}
+const printDogMsg = arr => {
+  arr.forEach(age => {
+    age >= 3
+      ? console.log(`Dog is Adult, with age: ${age}`)
+      : console.log('Dog is Puppy');
+  });
+};
 const checkDogs = (juliaDogs, kateDogs) => {
+  const newJuliaDogs = juliaDogs.slice(1, 4);
 
-    const newJuliaDogs = juliaDogs.slice(1, 4);
+  printDogMsg(newJuliaDogs);
+  printDogMsg(kateDogs);
+};
 
-    printDogMsg(newJuliaDogs);
-    printDogMsg(kateDogs);
-}
-
-checkDogs( [3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
