5979cb8817269a9f0f7f57a5454d7b4a9de69a3f
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2022-02-11 10:34:31 +0530

    challege 2 this section

--- 11-Arrays-Bankist/practice/challeges.js
+++ 11-Arrays-Bankist/practice/challeges.js
@@ -15,3 +15,25 @@
 // };
 
 // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+
+// challenge --- 2
+
+// const calHumanAge = (arr) => {
+
+//     const arrWithHumanAge = arr.map((item) => {
+
+//         return item > 2 ? (16 + item * 2) : (item * 2);
+//     }) 
+
+//     const arrFilteredAge = arrWithHumanAge.filter((item) => item > 18)
+
+//     const sumHumanAge = arrFilteredAge.reduce((acc, cur) => acc+cur);
+
+//     return sumHumanAge / arrFilteredAge.length;
+
+// }
+
+
+// console.log(calHumanAge([3, 5, 2, 12, 7]))
+// console.log(calHumanAge([4, 1, 15, 8, 3]))
