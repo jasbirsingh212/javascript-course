b2fc266e32adb2bfddbe0995e5bf40226a02aaa3
Jasbir Singh <jasbir212singh@gmail.com>
2023-02-28 13:44:26 +0530

    how to create and handle promises

--- 16-Asynchronous/practise/script.js
+++ 16-Asynchronous/practise/script.js
@@ -4,3 +4,15 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const createPromise = () =>
+  new Promise((resolve, reject) => {
+    reject('chal bsdk');
+  });
+
+createPromise()
+  .then(result => {
+    console.log(result);
+  })
+  .catch(err => {
+    console.log("error " + err);
+  });
