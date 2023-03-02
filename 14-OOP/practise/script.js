f2a21818be5b17c1bdba0b50601f934c619cab4c
Jasbir Singh <jasbir212singh@gmail.com>
2023-03-02 11:54:55 +0530

    oops contructor function in JS

--- 14-OOP/practise/script.js
+++ 14-OOP/practise/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+//we can not use arrow function for constructor functions
+//we can name it using first letter as small
+// do not create methods in CF due to performance issue
+
+const Person =  function (firstName, lastName) {
+    this.firstName = firstName,
+    this.lastName = lastName
+}
+
+const jasbir = new Person('Jasbir', 'Singh')
+console.log(jasbir)
