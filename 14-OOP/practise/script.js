f833595569f628a22e74f2a01789b39ef4b9332a
Jasbir Singh <jasbir212singh@gmail.com>
2023-03-02 12:10:15 +0530

    oops contructor function in JS 1

--- 14-OOP/practise/script.js
+++ 14-OOP/practise/script.js
@@ -4,10 +4,10 @@
 //we can name it using first letter as small
 // do not create methods in CF due to performance issue
 
-const Person =  function (firstName, lastName) {
-    this.firstName = firstName,
-    this.lastName = lastName
-}
+const Person = function (firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+};
 
-const jasbir = new Person('Jasbir', 'Singh')
-console.log(jasbir)
+const jasbir = new Person('Jasbir', 'Singh');
+console.log(jasbir);
