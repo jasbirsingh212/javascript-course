b82071d76a1337150fc62649cfe84f51a464a7a1
Jasbir Singh <jasbir212singh@gmail.com>
2023-05-01 12:00:28 +0530

    formatted code

--- 02-Fundamentals-Part-2/practice/script.js
+++ 02-Fundamentals-Part-2/practice/script.js
@@ -1,8 +1,6 @@
 // // //Cutshort assessment
 // // console.log("test" && "book");
 
-
-
 // // //next question
 // // const bird = {
 // //     size: 'small',
@@ -16,7 +14,6 @@
 // // //console.log(mouse.bird.size);
 // // console.log(mouse[bird.size]);
 // // console.log(mouse[bird['size']]);
-
 
 // //next question
 // // (function test() {
@@ -32,7 +29,6 @@
 // //     test();
 // //     console.log(a);
 // // })()
-
 
 // //next question
 
@@ -50,8 +46,6 @@
 
 // })()
 
-
-
 // //next question
 // const obj = { 1: 'a', 2: 'b', 3: 'c'};
 // const set = new Set([1,2,3,4,5]);
@@ -60,7 +54,6 @@
 // console.log(obj.hasOwnProperty(1));
 // //console.log(set.has('1'));
 // console.log(set.has(1))
-
 
 // //next question
 // const a = {};
@@ -73,55 +66,62 @@
 // console.log(a);
 // console.log(a[b]);
 
+//next question
 
-//next question 
-
-function foo(){
-    var f = (i) => arguments[0] + i;
-    return f(2);
+function foo() {
+  var f = (i) => arguments[0] + i;
+  return f(2);
 }
 
-function boo(){
-    var f = function(i){
-        arguments[0] + i;
-    }
+function boo() {
+  var f = function (i) {
+    arguments[0] + i;
+  };
 
-    return f(2);
+  return f(2);
 }
 // console.log(foo(10));
 // console.log(boo(10))
 
- const arr = [1, 2, 4, 5];
+const arr = [1, 2, 4, 5];
 // //arr=[1,2];//error
 // console.log(arr.push(6));//[1,2,4,5,6]
 // console.log(arr.unshift(3))
 // console.log(arr)
 // console.log(arr.indexOf(5))
 // console.log(arr.includes(2))
-console.log(arr[4])
+console.log(arr[4]);
 
 const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    age: 2037 - 1991,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven'],
-    calAge:function(){
-        console.log(this)
-        const abc = () => {
-            console.log(this)
-        }
-        abc()
-    }
-  };
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  calAge: function () {
+    console.log(this);
+    const abc = () => {
+      console.log(this);
+    };
+    abc();
+  },
+};
 //console.log(jonas.'last')
 
 //THIS KEYWORD
-console.log(this)
+console.log(this);
 
 const abc = () => {
-    console.log(this);
-}
+  console.log(this);
+};
 
 //abc()
 //jonas.calAge()
+
+const lastNameStr = "lastName";
+const jasbirObj = {
+  age: 23,
+  lastName: "singh",
+};
+
+console.log(jasbirObj.lastName);
