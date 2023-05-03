3dbac187d08301507edd9f069fbd04a1a9320771
Jasbir Singh <jasbir212singh@gmail.com>
2023-05-03 10:22:50 +0530

    undestanding scoping and scope chain

--- 08-Behind-the-Scenes/starter/test.js
+++ 08-Behind-the-Scenes/starter/test.js
@@ -1,76 +1,77 @@
-// // // // // //'use strict'
-// // // // // // //let and const can not be accessed outside of    //but var can be.
-// // // // // // function first () {
-// // // // // //     let a = 1;
+'use strict';
+//let and const can not be accessed outside of    //but var can be.
+// function first() {
+//   let a = 1;
 
-// // // // // //     // if(true){
-// // // // // //     //     const c = 10
-// // // // // //     // }
+//   if (true) {
+//     const c = 10;
+//   }
 
-// // // // // //     //console.log(c)
-// // // // // //    return  function second(){
-// // // // // //         console.log("Previous",a)
-// // // // // //         return a+=1;
-// // // // // //     }
-// // // // // // }
+//   //console.log(c); // undefined
+//   return function second() {
+//     console.log('Previous', a);
+//     return (a += 1);
+//   };
+// }
 
-// // // // // // const call1 = first()
-// // // // // // console.log(call1())
-// // // // // // console.log(call1())
-// // // // // // console.log(call1())
-// // // // // // console.log(call1())
-// // // // // // console.log(call1())
-// // // // // // console.log(call1())
-// // // // // // console.log(call1())
-// // // // // // console.log(call1())
+// const call1 = first();
+// console.log(call1());
+// console.log(call1());
+// console.log(call1());
+// console.log(call1());
+// console.log(call1());
+// console.log(call1());
+// console.log(call1());
+// console.log(call1());
 
-// // // // // // let obj = {}
+// let obj1 = {};
 
-// // // // // // if(Object.keys(obj).length === 0){
-// // // // // //     console.log(obj)
-// // // // // // }
+// if (Object.keys(obj1).length === 0) {
+//   console.log(obj1);
+// }
+// console.log("iojsdsicnjisjcnwc")
 
-// // // // // function first(){
+function first() {
+  console.log(firstName); // undefined
+  if (true) {
+    var firstName = 5;
+    console.log(firstName); // 5
+  }
+  console.log(firstName); //5
+}
 
-// // // // //     console.log(firstName)
-// // // // //     if(true){
-// // // // //         var firstName = 5
-// // // // //         console.log(firstName)
-// // // // //     }
-// // // // //     console.log(firstName)
-// // // // // }
+let firstName = 'jasbir';
+first();
+// let firstName = 'jasbir';
 
-// // // // // var firstName = "jasbir"
-// // // // // first()
+//  console.log(x); // undefined, But no error like x is not defined or can not find x
+//  var x  = 5;
+//  console.log(x)
 
-// // // //  console.log(x); // undefined, But no error like x is not defined or can not find x
-// // // //  var x  = 5;
-// // // //  console.log(x)
+// console.log(x); // Cannot access 'x' before initialization
+// let x  = 5;
 
-// // // // // console.log(x); // Cannot access 'x' before initialization
-// // // // // let x  = 5;
+// console.log(x); // Cannot access 'x' before initialization
+// const x  = 5;
 
-// // // // console.log(x); // Cannot access 'x' before initialization
-// // // // const x  = 5;
+// const Obj = {
+//   name: 'jasbir',
+//   age: 23,
+//   // @ts-ignore
+//   str: () => {
+//     console.log(this);
+//     return `${this.name}, ${this.age}`;
+//   },
+// };
 
-const Obj = {
-  name: 'jasbir',
-  age: 23,
-  // @ts-ignore
-  str: () => {
-    console.log(this);
-    return `${this.name}, ${this.age}`;
-  },
-};
+// console.log(Obj.str());
+// //'use strict'
 
-console.log(Obj.str());
-// // 'use strict'
+// function x () {
+//     console.log(this)
+// }
 
-// // // function x () {
-// // //     console.log(this)
-// // // }
-
-// // // x()
+// x()
 
 //  const first =  ()  => {
 
@@ -84,12 +85,12 @@ console.log(Obj.str());
 
 // first()
 
-// // // const x  = function () {
-// // //     console.log(this)
-// // // }
+// const x  = function () {
+//     console.log(this)
+// }
 
-// // // x()
+// x()
 
-// 'use strict'
-const x = () => console.log(this);
-x();
+// //'use strict'
+// const x = () => console.log(this);
+// x();
