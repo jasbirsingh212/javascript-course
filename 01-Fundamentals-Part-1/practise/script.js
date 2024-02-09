9d1fc7584ad6015ba48e39ec1e672f0a6c9211eb
Jasbir Singh <jasbir212singh@gmail.com>
2024-02-09 17:34:51 +0530

    completed asssignment 1

--- 01-Fundamentals-Part-1/practise/script.js
+++ 01-Fundamentals-Part-1/practise/script.js
@@ -5,11 +5,9 @@
 // const dolphineScore2 = 108;
 // const dolphineScore3 = 89;
 
-
 // const kolalaScore1 = 88 ;
 // const kolalaScore2 = 91;
 // const kolalaScore3 = 110;
-
 
 // const dolphineAvgScore = (dolphineScore1 + dolphineScore2 + dolphineScore3) / 3;
 // const koalasAvgScore = ( kolalaScore1 + kolalaScore2 + kolalaScore3)/3;
@@ -27,3 +25,19 @@
 //     else
 //     console.log(`No winner for this match`)
 // }
+
+//===============================================================================================================
+// Assignment 1
+
+const marksMass = 95;
+const markHeigth = 1.88;
+const marksBMI = marksMass / (markHeigth * markHeigth);
+
+const johnsMass = 85;
+const johnsHeigth = 1.76;
+const johnsBMI = johnsMass / (johnsHeigth * johnsHeigth);
+
+const markHasHigherBMI = marksBMI > johnsBMI;
+console.log(markHasHigherBMI, marksBMI, johnsBMI);
+
+//===================================================================================================================
