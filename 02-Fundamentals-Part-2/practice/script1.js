19bd5ce97a749472b93f9e332f04c83a4164def0
Jasbir Singh <jasbir212singh@gmail.com>
2024-02-14 09:56:06 +0530

    added strict mode

---
+++ 02-Fundamentals-Part-2/practice/script1.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let firstName = "Jasbir";
+let hasSurname = true;
+
+// if (hasSurame) {
+//   console.log(`${firstName} has surname`);
+// }
+
+if (hasSurname) {
+  console.log(`${firstName} has surname`);
+}
+
+// const interface = 'Audio';
+// const private = true;
+// const function = () => {};
+
+//========================================================================================================
