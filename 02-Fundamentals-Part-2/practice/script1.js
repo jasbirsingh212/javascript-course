fd6063b6082974c17690236a321bb0c5fd997f11
Jasbir Singh <jasbir212singh@gmail.com>
2024-02-19 11:09:55 +0530

    added coding challenge 2

--- 02-Fundamentals-Part-2/practice/script1.js
+++ 02-Fundamentals-Part-2/practice/script1.js
@@ -99,3 +99,14 @@ checkWinner(teamAScoreAvg, teamBScoreAvg);
 const arr = ["A", "B", "C", "D", 23];
 console.log(arr.indexOf("23"));
 console.log(arr.indexOf("d"));
+
+// ========================================================================================================
+
+//# coding challenge 2
+
+const calTip = (bill) => (bill >= 50 && bill <= 300 ? 0.15 : 0.2) * bill;
+console.log(calTip(100));
+const bills = [125, 555, 44];
+const tips = [calTip(bills[0]), calTip(bills[1]), calTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(tips, total);
