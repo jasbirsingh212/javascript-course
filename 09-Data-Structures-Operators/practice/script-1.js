2d3bba02def65b3fdb623a0971b354350cf03800
Jasbir Singh <jasbir212singh@gmail.com>
2024-03-09 10:49:16 +0530

    added code for swaping using destructuring

--- 09-Data-Structures-Operators/practice/script-1.js
+++ 09-Data-Structures-Operators/practice/script-1.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//swaping variables using destructuring
+let [x, y] = [1, 2];
+
+console.log(x, y);
+[y, x] = [x, y];
+
+console.log(x, y);
+
+//=================================================
