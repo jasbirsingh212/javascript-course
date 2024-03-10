3ca3330db79fc70d8442b9715ced8559ffc30c73
Jasbir Singh <jasbir212singh@gmail.com>
2024-03-10 10:13:19 +0530

    added nested destructuring

--- 09-Data-Structures-Operators/practice/script-1.js
+++ 09-Data-Structures-Operators/practice/script-1.js
@@ -17,29 +17,37 @@ console.log(x, y);
 // const { firstName: main = 'Jasbir1', lastName: secondary = 'Singh1' } = {};
 // console.log(main, secondary);
 
-// const restaurant = {
-//   name: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-//   openingHours: {
-//     thu: {
-//       open: 12,
-//       close: 22,
-//     },
-//     fri: {
-//       open: 11,
-//       close: 23,
-//     },
-//     sat: {
-//       open: 0, // Open 24 hours
-//       close: 24,
-//     },
-//   },
-// };
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
 
 // const { categories } = restaurant;
 // categories[0] = 'Jasbir';
 // console.log(categories, restaurant);
+
+//Nested Object destructuring
+// const {
+//   openingHours: {
+//     fri: { open, close },
+//   },
+// } = restaurant;
+// console.log(open, close);
