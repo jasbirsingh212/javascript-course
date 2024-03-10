9689320a6c77d982a50e51bdd24c362901686c7f
Jasbir Singh <jasbir212singh@gmail.com>
2024-03-10 10:03:25 +0530

    destructuring and object reference

--- 09-Data-Structures-Operators/practice/script-1.js
+++ 09-Data-Structures-Operators/practice/script-1.js
@@ -16,3 +16,30 @@ console.log(x, y);
 // const { firstName: main, lastName: secondary } = obj;
 // const { firstName: main = 'Jasbir1', lastName: secondary = 'Singh1' } = {};
 // console.log(main, secondary);
+
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+// };
+
+// const { categories } = restaurant;
+// categories[0] = 'Jasbir';
+// console.log(categories, restaurant);
