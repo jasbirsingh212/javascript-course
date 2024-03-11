1eecd39cce42f343023421982c64a088f419f967
Jasbir Singh <jasbir212singh@gmail.com>
2024-03-11 10:32:10 +0530

    short circuiting

--- 09-Data-Structures-Operators/practice/script-1.js
+++ 09-Data-Structures-Operators/practice/script-1.js
@@ -74,3 +74,77 @@ console.log(x, y);
 //   age: 26,
 //   // course: 'Software Engineering',
 // });
+
+//==============================================================
+
+//Spread operator
+
+//short-circuit
+// let num = 10;
+// console.log(3 || 'jasbir');
+// console.log(undefined || null);
+// num = num || 20;
+// console.log(num);
+
+// console.log(' ' && 'Jasbir');
+// console.log('J' && 1);
+// num = num && 20;
+// console.log(num);
+
+// coding challenge==============================================================
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//1
+const players1 = game.players[0];
+const players2 = game.players[1];
+console.log(players1, players2);
+
+//2
+const [gk1, ...fieldPlayers1] = players1;
+const [gk2, ...fieldPlayers2] = players2;
+console.log(gk1, fieldPlayers1);
+console.log(gk2, fieldPlayers2);
+
+//3
+const allPlayers = [...players1, players2];
+console.log(allPlayers);
